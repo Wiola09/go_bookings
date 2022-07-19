60934cd19870583dfddc25f0e627c988377d5e47
Tijana Dmitrovic-Ilijev <tijanadmi@yahoo.com>
2022-07-19 06:58:54 +0200

    add pictures

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/tijanadmi/go_bookings/pkg/config"
 	"github.com/tijanadmi/go_bookings/pkg/handlers"
-	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -17,6 +18,8 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	FileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", FileServer))
 
 	return mux
 }
