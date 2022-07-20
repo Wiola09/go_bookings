af760013e158a07d0c7288b6c1776a88d3cd84e4
Tijana Dmitrovic-Ilijev <tijanadmi@yahoo.com>
2022-07-20 14:54:37 +0200

    68 not finished

---
+++ internal/forms/forms.go
@@ -0,0 +1,45 @@
+package forms
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+//Form creates a custom form struct, embeds a url.Values object
+type Form struct {
+	url.Values
+	Errors errors
+}
+
+//Valid returns true if there are no errors, otherwise return false
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
+//New initializes a form struct
+func New(data url.Values) *Form {
+	return &Form{
+		data,
+		errors(map[string][]string{}),
+	}
+}
+
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+		}
+	}
+}
+
+//Has checks if form field is in post and not empty
+func (f *Form) Has(field string, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if x == "" {
+		f.Errors.Add(field, "This field cannot be blank")
+		return false
+	}
+	return true
+}
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/tijanadmi/go_bookings/internal/config"
+	"github.com/tijanadmi/go_bookings/internal/forms"
 	"github.com/tijanadmi/go_bookings/internal/models"
 	"github.com/tijanadmi/go_bookings/internal/render"
 )
@@ -70,9 +71,51 @@ func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
 
 // Reservation is the handler for the make-reservation page
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
 
 	// send data to the template
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+		Data: data,
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Email:     r.Form.Get("email"),
+		Phone:     r.Form.Get("phone"),
+	}
+
+	form := forms.New(r.PostForm)
+
+	//form.Has("first_name", r)
+
+	form.Required("first_name", "last_name", "email")
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+
+		render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+		return
+
+	}
+
 }
 
 // Availability renders the search availability page
